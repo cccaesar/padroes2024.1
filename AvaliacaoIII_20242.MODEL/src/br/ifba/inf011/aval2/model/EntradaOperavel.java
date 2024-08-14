1f75e769ac8f76319882bee1c19e9adfc3f248a1
cccaesar <ccsrrdrgscosta@gmail.com>
2024-08-14 12:30:38 -0300

    first commit

--- AvaliacaoIII_20242.MODEL/src/br/ifba/inf011/aval2/model/EntradaOperavel.java
+++
@@ -1,5 +0,0 @@
-package br.ifba.inf011.aval2.model;
-
-public interface EntradaOperavel extends Entrada, Operavel {
-
-}
--- AvaliacaoIII_20242.MODEL/src/br/ifba/inf011/aval2/model/Operavel.java
+++
@@ -1,9 +0,0 @@
-package br.ifba.inf011.aval2.model;
-
-public interface Operavel {
-	
-	public String dump();
-	public String ler(Credencial credencial) throws IllegalAccessException;
-	public void escrever(Credencial credencial, String escrever) throws IllegalAccessException;
-
-}
